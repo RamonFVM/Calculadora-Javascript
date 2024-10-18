19ce6b339b4c79b0ae97bbc95fc9df4f6cfe998e
Ramonfvm <ramonfhelipe547@gmail.com>
2024-10-18 18:10:05 -0300

    finalização da calculadora

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-const display = document.getElementById('display')
-const btn7 = document.getElementById('btn7')
-const btn8 = document.getElementById('btn8')
-const btn9 = document.getElementById('btn9')
-const btnDivide = document.getElementById('btnDivide')
-const btn4 = document.getElementById('btn4')
-const btn5 = document.getElementById('btn5')
-const btn6 = document.getElementById('btn6')
-const btnMultiply = document.getElementById('btnMultiply')
-const btn1 = document.getElementById('btn1')
-const btn2 = document.getElementById('btn2')
-const btn3 = document.getElementById('btn3')
-const btnSubtract = document.getElementById('btnSubtract')
-const btn0 = document.getElementById('btn0')
-const btnDecimal = document.getElementById('btnDecimal')
-const btnPercent = document.getElementById('btnPercent')
-const btnAdd = document.getElementById('btnAdd')
-const btnEqual = document.getElementById('btnEqual')
-const delet = document.getElementById('delet')
+const display = document.getElementById('display');
+const btn7 = document.getElementById('btn7');
+const btn8 = document.getElementById('btn8');
+const btn9 = document.getElementById('btn9');
+const btnDivide = document.getElementById('btnDivide');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+const btnMultiply = document.getElementById('btnMultiply');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btnSubtract = document.getElementById('btnSubtract');
+const btn0 = document.getElementById('btn0');
+const btnDecimal = document.getElementById('btnDecimal');
+const btnAdd = document.getElementById('btnAdd');
+const btnEqual = document.getElementById('btnEqual');
+const btnDelete = document.getElementById('delet');
 
 btn7.addEventListener('click', () => display.value += '7');
 btn8.addEventListener('click', () => display.value += '8');
@@ -32,7 +31,27 @@ btn3.addEventListener('click', () => display.value += '3');
 btnSubtract.addEventListener('click', () => display.value += ' - ');
 btn0.addEventListener('click', () => display.value += '0');
 btnDecimal.addEventListener('click', () => display.value += '.');
-btnPercent.addEventListener('click', () => display.value += ' % ');
 btnAdd.addEventListener('click', () => display.value += ' + ');
+btnDelete.addEventListener('click', () => display.value = '');
+
+btnEqual.addEventListener('click', () => {
+    try {
+        if (display.value.trim() === '') {
+            alert("Coloque um número!");
+            display.value = '';
+            return; 
+        }
+
+     
+        let expression = display.value
+            .replace(' x ', '*')
+            .replace(' ÷ ', '/');
 
 
+        const result = eval(expression);
+        display.value = result;
+    } catch (error) {
+        alert("Erro na expressão! Verifique se está digitando corretamente.");
+        display.value = ''
+    }
+});
